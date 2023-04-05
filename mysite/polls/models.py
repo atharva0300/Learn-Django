66c4a2e8f6ec3b5201d4f3fd888cef124563d2a6
Atharva Pingale <atharva0300@gmail.com>
2023-04-05 18:56:20 +0530

    tutorial part 2 updated

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Question(models.Model) : 
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField("date published")
+    
+    def was_published_recently(self) : 
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+
+
+class Choice(models.Model) : 
+    question = models.ForeignKey(Question , on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default = 0)
+
+
+# the Field class inside the models.Field, tells us what kind of data it holds. Ex- CharField, IntegerField, FloatField etc
+# the fields have different parameters depending on the type of the field
+# Django supports all the common database relationships: many-to-one, many-to-many, and one-to-one.
+
+# After creating the model, we need to create a Python-database API for accessing the Question and the Choice from the models.py 
+# add polls.apps.PollsConfig in the installed_apps , this does the above work
